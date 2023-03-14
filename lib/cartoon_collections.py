622b2aef1552c9e73934ae726145085f546e194d
Matthew Clark <afromatt6288@hotmail.com>
2023-03-13 20:34:27 -0600

    Lab Complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -3,18 +3,24 @@ def roll_call_dwarves(dwarves):
         print(f"{n+1}. {dwarves[n]}")
     pass
 
-def summon_captain_planet(planateer_calls):
+def summon_captain_planet(planeteer_calls):
     call_list = []
-    for calls in planateer_calls:
-        call_list.append(f"{calls}!")
-    print(call_list.capitalize)
-    # calls = [{call}.capitalize for call in planateer_calls]
-    # for call in planateer_calls:
-        # print(f"{call.capitalize}!")
+    for calls in planeteer_calls:
+        call_list.append(f"{calls.capitalize()}!")
+    return call_list
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) >= 5 :
+            return True
+    return False
     pass
 
-def find_the_cheese():
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for cheese in cheeses:
+        if cheese in snacks:
+            return cheese
+    return None        
     pass
